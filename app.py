d6e7d8967bd1ffc5aed2f9e296dfd5cd3f27e7fe
Jeffrey Wang <jeffwang125@gmail.com>
2024-04-17 06:58:32 -0400

    update url for explore page

--- app.py
+++ app.py
@@ -102,11 +102,11 @@ def index():
     # Display the index page.
     return render_template('index.html')
 
-@app.route('/my_content/', methods=['GET'])
+@app.route('/my_content', methods=['GET'])
 def contentListPage():
     return render_template('explore.html', softwares=softwares,languages=languages, courses=courses)
 
-@app.route('/explore/', methods=['GET'])
+@app.route('/explore', methods=['GET'])
 def explorePage():
     courses = random.sample(db_access.allCourses(),3)
     languages = random.sample(db_access.allLanguages(),3)
