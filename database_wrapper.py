f7803836552aa101ca94c086ccfba901058121d1
Mary <mjohn504@uncc.edu>
2024-04-19 15:15:15 -0400

    Debugged wrapper

--- database_wrapper.py
+++ database_wrapper.py
@@ -99,8 +99,8 @@ class DBAccess:
         return languages.order_by(self.Language.id).all()
 
     def addSoftware(self, name=None, site_url=None, download_url=None, documentation_url=None, description=None):
-        language = self.Language(name=name, site_url=site_url, download_url=download_url, documentation_url=documentation_url, description=description)
-        self.db.session.add(language)
+        software = self.Software(name=name, site_url=site_url, download_url=download_url, documentation_url=documentation_url, description=description)
+        self.db.session.add(software)
         self.db.session.commit()
 
     def setSoftware(self, id, name, site_url, download_url, documentation_url, description):
@@ -140,16 +140,15 @@ class DBAccess:
     # delete/edit support not implemented
 
     def addCoursePrerequisite(self, course_id, prerequisite_id):
-        course = self.db.session.get(self.Course, course_id)
-        prerequisite = self.db.session.get(self.Course, prerequisite_id)
-
+        course = self.Course.query.get_or_404(course_id)
+        prerequisite = self.Course.query.get_or_404(prerequisite_id)
         prerequisite.prerequisite_of.append(course)
-
+        
         self.db.session.commit()
 
     def addCourseLanguage(self, course_id, language_id):
-        course = self.db.session.get(self.Course, course_id)
-        language = self.db.session.get(self.Course, language_id)
+        course = self.Course.query.get_or_404(course_id)
+        language = self.Language.query.get_or_404(language_id)
 
         course.compatible_language.append(language)
 
@@ -159,8 +158,8 @@ class DBAccess:
         self.addCourseLangage(course_id, language_id)
 
     def addCourseSoftware(self, course_id, software_id):
-        course = self.db.session.get(self.Course, course_id)
-        software = self.db.session.get(self.Course, software_id)
+        course = self.Course.query.get_or_404(course_id)
+        software = self.Software.query.get_or_404(software_id)
 
         course.compatible_software.append(software)
 
@@ -170,8 +169,8 @@ class DBAccess:
         self.addCourseSoftware(course_id, software_id)
 
     def addLanguageSoftware(self, language_id, software_id):
-        language = self.db.session.get(self.Course, language_id)
-        software = self.db.session.get(self.Course, software_id)
+        language = self.Language.query.get_or_404(language_id)
+        software = self.Software.query.get_or_404(software_id)
 
         language.compatible_software.append(software)
 
