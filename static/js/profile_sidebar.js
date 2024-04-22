b3305624068ba2fa13032430ba37c831b2afb417
Jeffrey Wang <jeffwang125@gmail.com>
2024-04-22 06:42:56 -0400

    gauth works, now i need to finish login support

--- static/js/profile_sidebar.js
+++ static/js/profile_sidebar.js
@@ -1,15 +1,50 @@
-// change display based on logged in
-$(document).ready(function($)
-{
-    $(".signed-in").hide();
-    // if user cookie is present, show signed in
-        // $(".signed-out").hide();
-        // $(".signed-in").show();
+var jwtDecode;
+import("https://unpkg.com/jwt-decode@4.0.0/build/esm/index.js").then((module) => {
+    jwtDecode = module.jwtDecode;
 });
 
-// sign out button
-
 // Google OAuth
+function handleGToken(token) {
+    let payload = jwtDecode(token["credential"]);
+    const user_details = {};
+    user_details["user_id"] = payload["sub"]
+    user_details["email"] = payload["email"];
+    user_details["picture_url"] = payload["picture"];
+    user_details["name"] = payload["given_name"] + payload["family_name"];
+    // send server to create user if needed
+    // update cookie
+    update_view();
+}
+
+// TODO
+function update_user_cookie() {
+
+}
+
+// call once cookie state is changed
+function update_view() {
+    $(".signed-in").hide();
+    // access cookie
+    // update name in sidebar title and icon in navbar
+    // change which elements display based on whether user logged in
+}
+
+// initial update_view based on cookie
+$(document).ready(function($)
+{
+    update_view();
+});
+
+// sign out button functionality
+$(document).ready(function($)
+{
+    $("#signOutButton").click(function()
+    {
+        // remove cookie
+
+        update_view();
+    })
+});
 
 // reminder to jeffrey to write in support for the three other pages
 
